8fab8a8ea8f9b5524a1839eb53c299b72718f1e9
Margyysik <71218459+Margyysik@users.noreply.github.com>
2021-03-03 14:15:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (array) {
+		return array.length? array.sort((a,b)=>a-b)[0]:0
+	} else {
+		return 0
+	}
+  
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (array) {
+		return array.length? array.sort((a,b)=>a-b)[array.length-1]:0
+	} else{
+		return 0
+	}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (array) {
+		return array.length? array.reduce((acc,num)=>acc+num)/array.length:0
+	} else{
+		return 0
+	}
 }
